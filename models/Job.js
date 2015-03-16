35e36d88d961994972d968e8ab32b9c804570465
pkk82 <piotrkrzysztofkozak@gmail.com>
2015-03-16 19:05:47 +0000

    Add mongodb support

---
+++ models/Job.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+var jobSchema = mongoose.Schema({
+    title: {
+        type: String
+    },
+    description: {
+        type: String
+    }
+});
+
+var Job = mongoose.model('Job', jobSchema);
+
+exports.seedJobs = function() {
+    Job.find({}).exec(function(error, collection){
+        if (collection.length === 0) {
+            Job.create({title: 'Cook', description: 'You will be making bangels'});
+            Job.create({title: 'Waiter', description: 'You will be putting food on peplaces tables'});
+            Job.create({title: 'Programmer', description: 'You will be mindlessly typing for '});
+            Job.create({title: "Axe Maker", description: 'We need many axes made... so many...'});
+        }
+    })
+}
--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var jobModel = require('./models/Job');
+
 var app = express();
 app.set('views', __dirname);
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
-app.get('*', function(req, res) {
+
+
+
+app.get('/api/jobs', function(req, res) {
+    mongoose.model('Job').find({}).exec(function(error, collection) {
+        res.send(collection);
+    });
+});
+
+app.get('/', function(req, res) {
     res.render('index');
 });
+
+
+//mongoose.connect('mongodb://localhost/jobfinder');
+mongoose.connect('mongodb://jobfinder:jobfinder@ds061747.mongolab.com:61747/jobfinder')
+var con = mongoose.connection;
+con.once('open', function() {
+    console.log('connected to mongodb successfully!');
+    jobModel.seedJobs();
+});
+
 app.listen(process.env.PORT, process.env.IP);
